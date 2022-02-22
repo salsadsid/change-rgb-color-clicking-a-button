35b409847d2c44446147495879e1cf8437b05356
salsadsid <salman.dnj@gmail.com>
2022-02-22 14:14:36 +0600

    Copt Button

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,3 +11,13 @@ document.getElementById('btn').addEventListener('click', function () {
     bodyBg.style.backgroundColor = changeRgbColor();
     colorCode.innerText = changeRgbColor();
 })
+document.getElementById('copy-btn').addEventListener('click', function () {
+    const colorCode = document.getElementById('color-code');
+    const displayCopy = document.getElementById('display-copy');
+    navigator.clipboard.writeText(colorCode.innerText);
+    displayCopy.innerText = "  Copied!";
+})
+document.getElementById('copy-btn').addEventListener('mouseleave', function () {
+    const displayCopy = document.getElementById('display-copy');
+    displayCopy.innerText = "";
+})
